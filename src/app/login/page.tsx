0bb0cdb9b64a5773f94a74b1b74437eba5b3f891
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-07-26 15:17:17 +0530

    auth form template

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,44 @@
-export default function Login () {
-    return (
-        <div>
-            <h2>Login Page</h2>
-        </div>
-    )
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+  return (
+    <div className="py-8 flex flex-col justify-center items-center min-h-screen gap-3">
+      <h2 className="text-2xl">Login Form</h2>
+       <div className="flex flex-col mb-2 w-1/5">
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="email"
+          className="p-2 rounded-md outline-none text-slate-900"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+      </div>
+      <div className="flex flex-col mb-2 w-1/5">
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="password"
+          className="p-2 rounded-md outline-none text-slate-900"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+      </div>
+      <div className="flex items-center">
+      <button onClick={onLogin} className="bg-transparent border-slate-50 border-solid border rounded-md p-2 px-4 mr-4 hover:bg-slate-800">Login</button>
+      <Link className="underline " href={"/signup"}>Signup here</Link>
+      </div>
+    </div>
+  );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,56 @@
-export default function Signup () {
-    return(
-        <div>
-            <h2>Signup Page</h2>
-        </div>
-    )
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Signup() {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignUp = async () => {};
+  return (
+    <div className="py-8 flex flex-col justify-center items-center min-h-screen gap-3">
+      <h2 className="text-2xl">Signup Form</h2>
+      <div className="flex flex-col mb-2 w-1/5">
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          placeholder="username"
+          className="p-2 rounded-md outline-none text-slate-900 "
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+        />
+      </div>
+      <div className="flex flex-col mb-2 w-1/5">
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="email"
+          className="p-2 rounded-md outline-none text-slate-900"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+      </div>
+      <div className="flex flex-col mb-2 w-1/5">
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="password"
+          className="p-2 rounded-md outline-none text-slate-900"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+      </div>
+      <div className="flex items-center">
+      <button onClick={onSignUp} className="bg-transparent border-slate-50 border-solid border rounded-md p-2 px-4 mr-4 hover:bg-slate-800">Signup</button>
+      <Link className="underline " href={"/login"}>Login here</Link>
+      </div>
+    </div>
+  );
 }

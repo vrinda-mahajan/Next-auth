d77cf4fdb3ef28eaa03a6fb56a963f89e5e08d1d
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-07-27 14:08:09 +0530

    add signup post route

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,58 @@
+import connect from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import Joi from "joi";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+
+    const { error } = validate(reqBody);
+    if (error)
+      return NextResponse.json(
+        { error: "Request body is not valid!" },
+        { status: 400 }
+      );
+
+    const { username, email, password } = reqBody;
+    // checks if user already exists
+    const user = await User.findOne({ email });
+    if (user)
+      return NextResponse.json(
+        { error: "User already exists!" },
+        { status: 400 }
+      );
+
+    // hashing the password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    // creating new user
+    const userToBeSaved = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    // saving user to db
+    const savedUser = await userToBeSaved.save();
+    console.log(savedUser);
+    return NextResponse.json(
+      { message: "User created successfully!", success: true, savedUser },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+const validate = (user: any) => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(30).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(user);
+};
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 export default function ProfilePage() {
-    return(
-        <div>
-            <h2>User Profile</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h2>User Profile</h2>
+    </div>
+  );
 }

c4876ae56553171ce9bb2814d7b68510a2612f3c
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-07-30 21:28:16 +0530

    verify email page

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,20 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
-    const isPublicPath = (pathname === "/signup") || (pathname === "/login");
-    const token = request.cookies.get("token")?.value || "";
-    if(isPublicPath && token){
-        return NextResponse.redirect(new URL("/", request.nextUrl));
-    }
-    if(!isPublicPath && !token){
-        return NextResponse.redirect(new URL("/login", request.url));
-    }
-  
+  const pathname = request.nextUrl.pathname;
+  const isPublicPath =
+    pathname === "/signup" ||
+    pathname === "/login" ||
+    pathname === "/verifyemail";
+  const token = request.cookies.get("token")?.value || "";
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
 
 export const config = {
-  matcher: ["/", "/profile/:path*", "/signup", "/login"],
+  matcher: ["/", "/profile/:path*", "/signup", "/login", "/verifyemail"],
 };
